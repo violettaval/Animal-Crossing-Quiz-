7baea02a8293d244009c4df3c44e4cfe0084c090
violettaval <70643274+violettaval@users.noreply.github.com>
2020-09-29 00:41:53 -0500

    added an image populator and animal index selector

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Global Var
-
+var index;
     // refs the timer unit displayed on the screen 
 var counter = document.querySelector("#countdown");
-
+    // start button
+    var startButton = document.querySelector("#start");
     // function set the time at launch of game
 function setTime() {
     // set the timer to full
@@ -19,18 +20,28 @@ function setTime() {
     }, 1000);
 }
 
-    // start button
-var startButton = document.querySelector("#start");
+    //start button descriptors
 startButton.addEventListener("click", function (event) {
     event.stopPropagation;
         // hide the button
-        startButton.setAttribute('class', 'hidden');
-    
-    // starts the game
+    startButton.setAttribute('class', 'hidden');
+        // starts the game
     console.log("started game");
     setTime();
         // hide the button
     startButton.getAttribute('class', 'hidden');
-
+        // populate image
+    populate();
     // layoutStartUp();
 });
+
+    // sets up image    
+function populate() {
+
+    index = Math.floor(Math.random() * animal.length);
+    console.log("animal is " + animal[index].name);
+    var imgElements = document.querySelector("img");
+    imgElements.setAttribute("src", animal[index].image_url);
+    imgElements.setAttribute("alt", animal[index].name);
+    imgElements.removeAttribute("class");
+}
