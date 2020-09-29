12c6c09e87c6d93e950b760a2a9e7e6347f5e879
violettaval <70643274+violettaval@users.noreply.github.com>
2020-09-29 13:08:23 -0500

    Updated so Questions display

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+
 // Global Var
 var index;
     // refs the timer unit displayed on the screen 
 var counter = document.querySelector("#countdown");
     // start button
-    var startButton = document.querySelector("#start");
+var startButton = document.querySelector("#start");
+    // lets content be ref'd
+var content = document.querySelector("#content");
+    // Array holds all of the questions available
+var questionList = [
+    "What is this animal's birthday?",
+    "What is this animal's name?",
+    "What kind of animal is this?"
+];
     // function set the time at launch of game
 function setTime() {
     // set the timer to full
@@ -33,15 +42,30 @@ startButton.addEventListener("click", function (event) {
         // populate image
     populate();
     // layoutStartUp();
+    var h2 = document.getElementsByTagName("h2");
+    currentQuestion = questionSelector(questionList);
+    console.log(currentQuestion);
+    h2[0].textContent = (currentQuestion.toString());
+    console.log(h2);
+    // h2.append(currentQuestion);
+
 });
 
     // sets up image    
 function populate() {
-
     index = Math.floor(Math.random() * animal.length);
     console.log("animal is " + animal[index].name);
     var imgElements = document.querySelector("img");
     imgElements.setAttribute("src", animal[index].image_url);
     imgElements.setAttribute("alt", animal[index].name);
     imgElements.removeAttribute("class");
+}
+
+
+// question rotator
+function questionSelector(questionList) {
+    qIndex = Math.floor(Math.random() * 3);
+    console.log(qIndex);
+    console.log(questionList[qIndex]);
+    return (questionList[qIndex].toString());
 }
